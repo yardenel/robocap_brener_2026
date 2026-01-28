ac93df6234d12c89d54793d61fa0343e6ee5c0d4
szannon <szannon@proton.me>
2026-01-28 19:29:12 +0200

    vec2 impl

--- include/Vec2.hpp
+++ include/Vec2.hpp
@@ -1,4 +1,34 @@
 #pragma once
 
 template <typename T>
-class Vec2 {};
+class Vec2 {
+   public:
+    T x, y;
+    Vec2(const T& x, const T& y);
+    Vec2(const Vec2<T>& other) = default;
+    Vec2(Vec2<T>&& other)      = default;
+}
+
+template <typename T>
+Vec2<T>& Vec2<T>::operator=(const Vec2<T>& other) {
+    x = other.x;
+    y = other.y;
+    return *this;
+}
+
+Vec2<T>& operator=(Vec2<T>&& other) {
+    x = std::move(other.x);
+    y = std::move(other.y);
+    return *this;
+}
+
+Vec2<T> operator+(const Vec2<T>& other) { return Vec2(x + other.x, y + other.y); }
+
+Vec2<T> operator-(const Vec2<T>& other) { return Vec2(x - other.x, y - other.y); }
+
+Vec2<T> operator*(const Vec2<T>& other) { return Vec2(x * other.x, y * other.y); }
+
+Vec2<T> operator/(const Vec2<T>& other) {
+    return Vec2(x / other.x, y / other.y);
+
+    ;

da405ffdae5f66e8c0244460b4f99562b5f246a7
szannon <szannon@proton.me>
2026-01-28 19:03:33 +0200

    dummy loop

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,10 @@
-#include "Vec2.hpp"
+#include "Arduino.h"
 
-void setup() { ; }
+void setup() { pinMode(LED_BUILTIN, OUTPUT); }
 
-void loop() { ; }
+void loop() {
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(2000);
+    digitalWrite(LED_BUILTIN, LOW);
+    delay(2000);
+}

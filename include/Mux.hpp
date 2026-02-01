da280a73af2946e42894b8e35825fe911e89d873
szannon <szannon@proton.me>
2026-02-01 18:32:35 +0200

    implemented mux interface

---
+++ include/Mux.hpp
@@ -0,0 +1,17 @@
+#pragma once
+#include <Arduino.h>
+
+class Mux {
+   private:
+    const uint8_t sig, s0, s1, s2, s3;
+
+   public:
+    Mux() = delete;
+    Mux(const uint8_t sig_pin,
+        const uint8_t s0_pin,
+        const uint8_t s1_pin,
+        const uint8_t s2_pin,
+        const uint8_t s3_pin);
+
+    int read_at(size_t idx);
+}

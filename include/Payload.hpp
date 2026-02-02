fc9fee5a32ed05f925736cd805790b1fd64f35d8
szannon <szannon@proton.me>
2026-02-02 16:39:18 +0200

    updated Payload interface

--- include/Payload.hpp
+++ include/Payload.hpp
@@ -1,4 +1,6 @@
 #pragma once
+
+#include "Mux.hpp"
 #include "Vec2f.hpp"
 
 class Payload {
@@ -10,6 +12,7 @@ class Payload {
     static constexpr const int muxS3      = 20;
     static constexpr const int mux_SIG    = 24;
 
+    Mux m_mux;
     Vec2f m_pos;
 
    public:

6b42588cb0e0eeb1c441deabb966253c57cdd8a4
szannon <szannon@proton.me>
2026-02-19 12:38:36 +0200

    updated consts

--- include/Payload.hpp
+++ include/Payload.hpp
@@ -8,21 +8,21 @@
 class Payload {
    private:
     static constexpr const int SERIAL_NUM = 115200;
-    static constexpr const int MUX_S0     = 23;
-    static constexpr const int MUX_S1     = 22;
-    static constexpr const int MUX_S2     = 21;
-    static constexpr const int MUX_S3     = 20;
-    static constexpr const int MUX_SIG    = 24;
+    static constexpr const int MUX_S0     = 31;
+    static constexpr const int MUX_S1     = 30;
+    static constexpr const int MUX_S2     = 29;
+    static constexpr const int MUX_S3     = 28;
+    static constexpr const int MUX_SIG    = 32;
 
     static constexpr const int ENGINES_FREQUENCY = 20000;
 
     /// NOTE: the color sensor's addr is 0x29
 
     /// TODO: update these values to be correct.
-    static constexpr const int LF_FORWARD = 30, LF_BACKWARD = 29, LF_PWM = 25;
-    static constexpr const int LR_FORWARD = 31, LR_BACKWARD = 35, LR_PWM = 26;
-    static constexpr const int RF_FORWARD = 32, RF_BACKWARD = 44, RF_PWM = 27;
-    static constexpr const int RR_FORWARD = 33, RR_BACKWARD = 45, RR_PWM = 28;
+    static constexpr const int LF_FORWARD = 13, LF_BACKWARD = 41, LF_PWM = 23;
+    static constexpr const int LR_FORWARD = 38, LR_BACKWARD = 35, LR_PWM = 37;
+    static constexpr const int RF_FORWARD = 40, RF_BACKWARD = 39, RF_PWM = 22;
+    static constexpr const int RR_FORWARD = 34, RR_BACKWARD = 33, RR_PWM = 36;
 
     static constexpr const int TCA_ADDR = 0x70;
 
